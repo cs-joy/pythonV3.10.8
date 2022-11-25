47a2e078bbb3f9462a5432b9e99d6dcccf0ff9d2
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 05:43:30 +0600

    Update app.py

---
+++ drawing_a_shape/app.py
@@ -0,0 +1,15 @@
+# print triangle
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# the order of the instructions actually matters a lot
+
+#print("/___|")
+#print("   /|")
+#print("  / |")
+#print(" /  |")
+
+

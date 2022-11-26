c836e1b902f5cd46726d57e278f893c748cf6032
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 16:33:12 +0600

    Update app.py

--- working_with_numbers/app.py
+++ working_with_numbers/app.py
@@ -5,3 +5,32 @@ print(-2)
 print(2.345)
 
 print(23456.4567)
+
+# arithmatic operations
+print(2 + 4)
+print(2 + 4.06)
+
+print(2 / 4)
+
+print(4.15 * 2.0)
+
+print(4 - 10.4)
+print(5 - 7)
+
+# specific order operation or say complex operation
+print(2 * 5 + 2) # first 2 * 5 then 10 + 2 = 12
+
+print(2 * (5 + 2)) # first 5 + 2 then 2 * 7 = 14
+
+# modulus operator
+print(11 % 3)
+
+# with varibale
+my_num = 5
+print(my_num % 2)
+
+# convert the number into a string
+print(str(my_num) + ' is my favorite number.')
+
+# when i wasn't convert the number to a string then python give us error and say...
+# print(my_num + " is my favorite number.")

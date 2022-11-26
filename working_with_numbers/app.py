1d156ca60c36559fffb1b476b9120b3ff86eb80e
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 16:57:38 +0600

    Update app.py

--- working_with_numbers/app.py
+++ working_with_numbers/app.py
@@ -1,3 +1,5 @@
+from math import * # basically this gonna to do is go out and grab math functions
+
 print(2)
 
 print(-2)
@@ -34,3 +36,40 @@ print(str(my_num) + ' is my favorite number.')
 
 # when i wasn't convert the number to a string then python give us error and say...
 # print(my_num + " is my favorite number.")
+
+num = -2
+# abs() - give us absolute value
+print(abs(num)) # expected 2
+
+
+# power
+# pow() - 2 parameter required
+# so, first parameter hold number and seocnd parameter hold the power that i want to take the number to
+print(pow(5, 2)) # 5^2 (five square)
+
+# max() - find out max number
+print(max(3, 7))
+
+# min() - find out min number
+print(min(3, 7))
+
+# round() - this is gonna allow us to round a number
+print(round(9.4))
+print(round(9.5))
+
+
+# using the math modules or "from math import *"
+
+# floor() - basically it just grab the lowest number
+# it is essentially chop up the decimal point
+print(floor(3.9))
+
+# ceil() - opposite of floor()
+print(ceil(3.9))
+
+# square root
+# sqrt() - essentially this return the square root of the number
+print(sqrt(144))
+
+# there have a lot of functions inside of the math modules, so go on google and search, how do they work?
+# !bye

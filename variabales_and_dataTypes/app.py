4a883491bac86f655e633465f1b88d98e6bc58d8
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 06:45:23 +0600

    initial point

---
+++ variabales_and_dataTypes/app.py
@@ -0,0 +1,27 @@
+# varibale: basically it's just container where we can store certain data values
+#           and we use a variable put those data values inside containers, it makes a lot easier for us to
+#           work with and manage all of the different data inside of our programs
+# 
+#           so, variables are extremely helpful and its like core topic in python, you really go want to know in Master
+
+character_name = "Tom" # storing string
+                       # a string is basically just plain text
+
+character_age = "20"   # string
+#character_age = 20    # numbers (decimal or any other types are included in python, i.e: 20.4387546)
+
+# boolean
+# so much important
+is_Male = True # either True or False are valid in boolean data types
+
+# so basic data types are:
+#     - string
+#     - numbers
+#     - boolean
+
+print("There onece was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+
+character_name = "Joy"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")

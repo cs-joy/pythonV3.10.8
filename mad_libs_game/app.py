0e671506bf69616862a356a60879979ae199b567
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 18:38:36 +0600

    Create app.py

---
+++ mad_libs_game/app.py
@@ -0,0 +1,11 @@
+
+# mad libs game - i mad libs game is basically just a game where you can entering a bunch of word(verb, nouns, adjectives etc.)
+#                so, you basically takes all of words and put them into a story randomly
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Rose are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)

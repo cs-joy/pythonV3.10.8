10531bcd15db03149cb17acf9d203f85ff25e695
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-26 19:14:04 +0600

    Create app.py

---
+++ lists/app.py
@@ -0,0 +1,8 @@
+
+# in python when you dealing with large amount of data
+# then you want to make sure that manage it and organize it properly
+
+# A list is just a structure that we can use inside of python to stored a list of the informations
+# so, we can take a bunch of different data values we can put them inside a list and its allows us to 
+# organize them and keep track them a lot easier
+

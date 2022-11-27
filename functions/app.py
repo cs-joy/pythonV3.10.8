30227e19f4f3b0e74602c8f8dd88ed339a8ad941
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-27 22:09:04 +0600

    Update app.py

--- functions/app.py
+++ functions/app.py
@@ -1,8 +1,13 @@
 # function - a function is basically just a collection of code which performs a specific tasks
 
-def sayhi():
-    print("Hello World")
+def sayhi(name, age):
+    print("Hello " + name + ", you are " + age)
 
-print("Top")
-sayhi()
-print("Bottom")
+
+sayhi("Joy", "23")
+sayhi("Mike", "27")
+
+def int_str(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+int_str("John", 30)

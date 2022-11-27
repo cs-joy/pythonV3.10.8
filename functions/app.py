f36c126262f4bab1edefa2c4124f42bf8669d579
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-27 21:43:44 +0600

    initial point

--- functions/app.py
+++ functions/app.py
@@ -1,1 +1,8 @@
-# source: https://www.youtube.com/watch?v=rfscVS0vtbw&t=227s
+# function - a function is basically just a collection of code which performs a specific tasks
+
+def sayhi():
+    print("Hello World")
+
+print("Top")
+sayhi()
+print("Bottom")

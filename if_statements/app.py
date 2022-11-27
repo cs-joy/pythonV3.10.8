9632ed447a2523b6c8c33452f18271445982d5c4
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-27 22:35:19 +0600

    Create app.py

---
+++ if_statements/app.py
@@ -0,0 +1,20 @@
+"""
+I wake up
+If i'm hungry
+    I eat breakfast
+
+I leave my house
+If it's cloudy
+    I bring an umbrella
+Otherwise
+    I bring sunglass
+
+I'm at a restaurent
+If i want meat
+    I order a steak
+Otherwise If i want pasta
+    I order spaghetti & metaballs
+Otherwise
+    I order salad
+"""
+

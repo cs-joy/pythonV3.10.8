78f5fff148f30af9bb107a94f07d312669c6e2aa
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-29 23:54:22 +0600

    Create app.py

---
+++ dictionaries/app.py
@@ -0,0 +1,17 @@
+# dictionaries - a dictionaries is a special structure in python which allows us 
+# to store informations called `key` `value` pairs
+
+monthConversions = {
+    "Jan": "January", # `key: value` note- all of the keys have to be unique
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}

f3fd860b7e6779ad411b78f772f449ca3bf8d3bd
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-29 23:02:57 +0600

    Create app.py

---
+++ building_a_better_calculator/app.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '*':
+    print(num1 * num2)
+elif operator == '/':
+    print(num1 / num2)
+else:
+    print("Invalid operator!")

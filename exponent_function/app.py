0b37714852b6a5b22153420f4e3eee7570316581
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-30 22:17:39 +0600

    build `power()`
    *exponent function

--- exponent_function/app.py
+++ exponent_function/app.py
@@ -3,3 +3,14 @@
 # An exponent function is basically gonna allow us to take a certain number and raise it to a specific power
 
 print(2 ** 3) # 2^3
+
+# going to build exponent function
+def raise_to_power(base_num, power_num):
+    result = 1
+    for index in range(power_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 2))
+
+# thus the basics of build power function/ exponent function

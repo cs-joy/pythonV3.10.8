cdd9aa1c16037e38d5c0e62f9f18a31b7eccabf7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-30 09:02:25 +0600

    initial point

---
+++ while_loop/app.py
@@ -0,0 +1,11 @@
+# while - a while loop is basically is a structure in python which allows us
+# to loop through and execute a block of code multiple times
+
+i = 1
+while i<= 10:
+    print(i)
+    i += 1
+
+print("Done with loop")
+
+# thus the basics of while loop

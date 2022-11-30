d0b35bd9ed60f658014af92ef838ba76b96f7bbc
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-11-30 21:57:35 +0600

    initial point of `exponent function`

---
+++ exponent_function/app.py
@@ -0,0 +1,5 @@
+# exponent function
+# # how to build exponent function?
+# An exponent function is basically gonna allow us to take a certain number and raise it to a specific power
+
+print(2 ** 3) # 2^3

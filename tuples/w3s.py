b4f82cb67d4d32041fffcf51972bd6ff0509e8a3
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-01 09:44:06 +0600

    ques explanation

--- tuples/w3s.py
+++ tuples/w3s.py
@@ -39,3 +39,35 @@ if "Joy" in employeer:
     print("Yes, `Joy` is in the employeer tuple") # return the index
 else:
     print("No found!")
+
+
+#question 01
+my_friends = ("tom", "jety", "john", "doe", "mike", "scarry", "lotus", "keily", "mark")
+print(my_friends[2:])
+# isIncluded element in index 2 or not?
+# output:: ('john', 'doe', 'mike', 'scarry', 'lotus', 'keily', 'mark')
+# so, isIncluded = True
+
+# for negative index
+print(my_friends[-4:])
+# isIncluded element in index -4 or not?
+# output:: ('scarry', 'lotus', 'keily', 'mark')
+# so, isInclude = True
+
+
+
+# Range
+print(my_friends[2:7])
+# isIncluded element in index 2 or not?
+# isIncluded_2 = True
+## output:: ('john', 'doe', 'mike', 'scarry', 'lotus')
+# isIncluded element in index 7 or not?
+# so, isIncluded_7(last element in the range) = False
+
+# negative
+print(my_friends[-6:-3])
+# isIncluded element in index -6 or not?
+# isIncluded(-6) = True
+## output:: ('doe', 'mike', 'scarry')
+# isIncluded element in index 7 or not?
+# so, isIncluded(-3)(last element in the range) = False

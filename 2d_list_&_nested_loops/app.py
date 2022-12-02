5e45c8dd384d24101b278c8ea4c44f1fdbcd97de
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 09:30:29 +0600

    completed to write basics of `2d & nested loops`

--- 2d_list_&_nested_loops/app.py
+++ 2d_list_&_nested_loops/app.py
@@ -1,1 +1,32 @@
 # s: https://www.youtube.com/watch?v=rfscVS0vtbw&t=10032s
+
+# 2d list
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[0][0]) # out/ 1
+
+print(number_grid[2][1]) # out/ 8
+
+
+# nested loop
+
+for row in number_grid:
+    print(row)
+
+    # out::
+    """
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+    [0]
+    """
+    # i want to access each of these individual attributes inside of each of these array elements
+
+for row in number_grid:
+    for col in row:
+        print(col)

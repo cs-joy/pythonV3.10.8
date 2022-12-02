865090a3e4586280b5437653d3161e3929a28d88
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 09:49:41 +0600

    Update app.py

--- build_a_translator/app.py
+++ build_a_translator/app.py
@@ -3,7 +3,19 @@
 def translator(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
+        if letter in "AEIOUaeiou": # not efficient
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translator(input("Enter a phrase: ")))
+
+
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou": # efficient
             translation = translation + "g"
         else:
             translation = translation + letter

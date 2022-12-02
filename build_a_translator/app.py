e1a507fe3596d909c899a30e0fc3cba0dcbdc7e5
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 09:44:24 +0600

    initialized

---
+++ build_a_translator/app.py
@@ -0,0 +1,12 @@
+
+
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translator(input("Enter a phrase: ")))

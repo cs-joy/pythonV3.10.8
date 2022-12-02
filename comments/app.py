b089c6fff13feb6fd33991d2c55357f711e0bd63
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 16:43:00 +0600

    Create app.py

---
+++ comments/app.py
@@ -0,0 +1,16 @@
+# comments - a comment is basically a line inside of our python file that just not gonna rendered by python
+# comments is basically used for human
+
+# this prints out a string
+
+# there are to way to comments in python file
+# first is hash tag(#) for single line comment
+# print("Comments are fun")
+# and second is triple quotation mark for multi-line comment
+'''
+this is a multi-line
+comments in python
+'''
+
+
+print("Comments are fun")

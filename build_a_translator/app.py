353e238b8e69c1eda85d4dc63874111533254c02
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 09:54:53 +0600

    more functional :)
    * little bit better

--- build_a_translator/app.py
+++ build_a_translator/app.py
@@ -1,5 +1,5 @@
-
-
+"""
+# v1
 def translator(phrase):
     translation = ""
     for letter in phrase:
@@ -10,13 +10,33 @@ def translator(phrase):
     return translation
 
 print(translator(input("Enter a phrase: ")))
+"""
 
 
+"""
+# v2
 def translator(phrase):
     translation = ""
     for letter in phrase:
         if letter.lower() in "aeiou": # efficient
             translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translator(input("Enter a phrase: ")))
+"""
+
+
+# v3
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou": # efficient
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
         else:
             translation = translation + letter
     return translation

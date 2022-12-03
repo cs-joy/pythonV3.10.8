40edfcbbee307e931ebdeef7a8b2bfc1e8ea1478
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 23:07:37 +0600

    Update app.py

--- python_interpreter/app.py
+++ python_interpreter/app.py
@@ -1,3 +1,8 @@
 # python interpreter - A python interpreter is basically a little enironment that we can use to 
 # executing python commands
 
+# open up `cmd` on windows, `terminal` in mac or linux
+
+# command - `python`
+
+# it's essentially just in environment where we can test out python code

5e17220cfd2978463430a29b04dd57152e4de8d4
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 21:31:43 +0600

    definition of class and object

--- classes_and_objects/Student.py
+++ classes_and_objects/Student.py
@@ -1,3 +1,7 @@
-class Student:
+class Students:
     # initialize
     def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
--- classes_and_objects/app.py
+++ classes_and_objects/app.py
@@ -1,3 +1,7 @@
-# 
+from Student import Students
 
-# call
+student1 = Students("Jim", "CSE", 3.65, False)
+print(student1.major)
+
+student2 = Students("Kalvin", "Art", 2.74, False)
+print(student2.name)

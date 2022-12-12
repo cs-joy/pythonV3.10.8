8c040bfd7c067ddf1a682d50d95e570f516a187f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-12 23:55:02 +0600

    Update app.py

--- docs/data_structures/lists/app.py
+++ docs/data_structures/lists/app.py
@@ -27,3 +27,28 @@ list.reverse()
 '''
 fruits.reverse()
 print(fruits)
+
+
+'''
+list.append(x)
+     Add an item to the end of the list. Equivalent to a[len(a):] = [x].
+'''
+fruits.append('grape')
+print(fruits)
+
+
+'''
+list.sort(*, key=None, reverse=False)
+     Sort the items of the list in place (the arguments can be used for sort customization, see sorted() for their explanation).
+'''
+fruits.sort()
+print(fruits)
+
+
+'''
+list.pop([i])
+     Remove the item at the given position in the list, and return it. If no index is specified, a.pop() removes and returns the last item in the list. (The square brackets around the i in the method signature denote that the parameter is optional, not that you should type square brackets at that position. 
+     You will see this notation frequently in the Python Library Reference.)
+'''
+print(fruits.pop(5))
+print(fruits)

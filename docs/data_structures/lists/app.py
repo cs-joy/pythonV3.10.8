af200ba685f860eb44f4b630f3e5ed2721dd78e7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-12 23:38:15 +0600

    Create app.py

---
+++ docs/data_structures/lists/app.py
@@ -0,0 +1,7 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+'''
+list.count(x)
+     Return the number of times x appears in the list.
+'''
+print(fruits.count('apple'))

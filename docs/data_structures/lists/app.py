09369c26360fb1182c16248f0807501846f3b2f7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-13 21:01:49 +0600

    `insert()` added to the dt list

--- docs/data_structures/lists/app.py
+++ docs/data_structures/lists/app.py
@@ -89,4 +89,14 @@ print(fruits)
 
 
 
+'''
+list.insert(i, x)
+      Insert an item at a given position. 
+      The first argument is the index of the element before which to insert, so a.insert(0, x) inserts at the front of the list, and a.insert(len(a), x) is equivalent to a.append(x).
+'''
+fruits.insert(10, 'jim') # after 'mike'
+print(fruits)
+
+
+
 #### ref:: https://docs.python.org/3/tutorial/datastructures.html

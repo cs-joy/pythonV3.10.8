9b8c7f45120f3b95e540747eac5bcc893ad7d1f8
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-13 21:34:34 +0600

    Update app.py

--- docs/data_structures/lists/Comprehensions/app.py
+++ docs/data_structures/lists/Comprehensions/app.py
@@ -11,3 +11,11 @@ squares = []
 for x in range(10):
     squares.append(x**2)
 print(squares) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+###  this creates (or overwrites) a variable named x that still exists after the loop completes. 
+#    We can calculate the list of squares without any side effects using:
+squareList = list(map(lambda x: x**2, range(10)))
+print(squareList) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# or, equivalently:
+square = [x**2 for x in range(10)]
+print(square) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]

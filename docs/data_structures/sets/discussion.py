a9eed143474907350bbe68751f187fa5cfb6d44c
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 23:57:53 +0600

    Create discussion.py

---
+++ docs/data_structures/sets/discussion.py
@@ -0,0 +1,9 @@
+'''
+
+Python also includes a data type for sets. A set is an unordered collection with no duplicate elements. Basic uses include membership testing and eliminating duplicate entries. Set objects also support mathematical operations like union, intersection, difference, and symmetric difference.
+
+Curly braces or the set() function can be used to create sets. Note: to create an empty set you have to use set(), not {}; the latter creates an empty dictionary, a data structure that we discuss in the next section.
+
+Here is a brief demonstration:
+
+'''

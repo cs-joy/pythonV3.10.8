360964525f101c95a9ab77495726196fba1599fb
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-01 00:03:23 +0600

    Update discussion.py

--- docs/data_structures/sets/discussion.py
+++ docs/data_structures/sets/discussion.py
@@ -19,12 +19,12 @@ print('crabgrass' in basket)
 
 a = set('abracadabra')
 b = set('alacazam')
-a                                  # unique letters in a
-{'a', 'r', 'b', 'c', 'd'}
-a - b                              # letters in a but not in b
-{'r', 'd', 'b'}
-a | b                              # letters in a or b or both
-{'a', 'c', 'r', 'd', 'b', 'm', 'z', 'l'}
-a & b                              # letters in both a and b
-{'a', 'c'}
-a ^ b                              # letters in a or b but not both
+print(a)                                  # unique letters in a
+
+print(a - b)                              # letters in a but not in b
+
+print(a | b)                              # letters in a or b or both
+
+print(a & b)                              # letters in both a and b
+
+print(a ^ b)                              # letters in a or b but not both

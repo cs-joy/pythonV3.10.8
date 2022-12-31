acfac6587601f55d49b535d09e31ead7ab9a66e0
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 23:57:25 +0600

    Update discussion.py

--- docs/data_structures/Tuples and Sequences/discussion.py
+++ docs/data_structures/Tuples and Sequences/discussion.py
@@ -15,3 +15,15 @@ print(nested)
 # but they can contain mutable objects:
 v = ([1, 2, 3], [3, 2, 1])
 print(v)
+
+'''
+
+As you see, on output tuples are always enclosed in parentheses, so that nested tuples are interpreted correctly; they may be input with or without surrounding parentheses, although often parentheses are necessary anyway (if the tuple is part of a larger expression). It is not possible to assign to the individual items of a tuple, however it is possible to create tuples which contain mutable objects, such as lists.
+
+Though tuples may seem similar to lists, they are often used in different situations and for different purposes. Tuples are immutable, and usually contain a heterogeneous sequence of elements that are accessed via unpacking (see later in this section) or indexing (or even by attribute in the case of namedtuples). Lists are mutable, and their elements are usually homogeneous and are accessed by iterating over the list.
+
+A special problem is the construction of tuples containing 0 or 1 items: the syntax has some extra quirks to accommodate these. Empty tuples are constructed by an empty pair of parentheses; a tuple with one item is constructed by following a value with a comma (it is not sufficient to enclose a single value in parentheses). Ugly, but effective. For example:
+
+'''
+
+empty = ()

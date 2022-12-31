ddbc1fa47814a7105b139d4c33af357d1ad8841a
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 23:54:02 +0600

    Update discussion.py

--- docs/data_structures/Tuples and Sequences/discussion.py
+++ docs/data_structures/Tuples and Sequences/discussion.py
@@ -1,5 +1,12 @@
 # A tuple consists of a number of values separated by commas, for instance:
 
-t = [23145, 76432, 4256, 'hello', 765, 'world', 'hello world', 8765, 'through t']
+t = 23145, 76432, 4256, 'hello', 765, 'world', 'hello world', 8765, 'through t'
 print(t)
 print(t[8])
+
+# Tuples may be nested:
+nested = t, (123, 654, 321)
+print(nested)
+
+# Tuples are immutable
+nested[3] = 56789 # 'tuple' object does not support item assignment

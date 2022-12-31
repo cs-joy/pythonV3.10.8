578d7d5e3c260b2523921c0cbcfd51489d03d02d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-31 23:49:44 +0600

    Create discussion.py

---
+++ docs/data_structures/Tuples and Sequences/discussion.py
@@ -0,0 +1,5 @@
+# A tuple consists of a number of values separated by commas, for instance:
+
+t = [23145, 76432, 4256, 'hello', 765, 'world', 'hello world', 8765, 'through t']
+print(t)
+print(t[8])

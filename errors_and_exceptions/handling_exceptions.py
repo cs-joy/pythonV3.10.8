1ff7d9e7586e08c1b3bfbe626d8550c6588f6e13
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-05 23:18:36 +0600

    Update handling_exceptions.py

--- errors_and_exceptions/handling_exceptions.py
+++ errors_and_exceptions/handling_exceptions.py
@@ -76,12 +76,13 @@ For convenience, builtin exception types define __str__() to print all the argum
 '''
 
 try:
-    raise Exception('spam', 'egg')
+    raise Exception('spam', 'egg', 'args')
 except Exception as inst:
     print(type(inst))    # the exception instance
     print(inst.args)     # arguments stored in .args
     print(inst)          # __str__ allows args to be printed directly,
                          # but may be overridden in exception subclasses
-    x, y = inst.args     # unpack args
+    x, y, z = inst.args     # unpack args
     print('x =', x)
     print('y =', y)
+    print('z =', z)

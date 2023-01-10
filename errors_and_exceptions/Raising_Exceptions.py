2d565b05a6f0e83ffde78509f293ad8933015a01
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-10 23:42:19 +0600

    Update Raising_Exceptions.py

--- errors_and_exceptions/Raising_Exceptions.py
+++ errors_and_exceptions/Raising_Exceptions.py
@@ -16,5 +16,19 @@ If an exception class is passed, it will be implicitly instantiated by calling i
 
 
 '''
+If you need to determine whether an exception was raised but don’t intend to handle it, 
+a simpler form of the raise statement allows you to re-raise the exception:
+'''
 
+try:
+    raise ValueError('Hello World')
+except ValueError:
+    print('An exception flew by!')
+    raise
+'''
+An exception flew by!
+Traceback (most recent call last):
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\Raising_Exceptions.py", line 24, in <module>
+    raise ValueError('Hello World')
+ValueError: Hello World
 '''

3a7d443a5100773b5b74584968adf5d8c7638dfc
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-10 23:50:25 +0600

    Update ExceptionChaining.py

--- errors_and_exceptions/ExceptionChaining.py
+++ errors_and_exceptions/ExceptionChaining.py
@@ -7,8 +7,21 @@ it will have the exception being handled attached to it and included in the erro
 try:
     open('database.txt')
 except OSError:
-    print('file not found')
+    raise RuntimeError('Unable to handle error')
 
+'''
+Traceback (most recent call last):
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\ExceptionChaining.py", line 8, in <module>
+    open('database.txt')
+FileNotFoundError: [Errno 2] No such file or directory: 'database.txt'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\ExceptionChaining.py", line 10, in <module>
+    raise RuntimeError('Unable to handle error')
+RuntimeError: Unable to handle error
+'''
 
 
 

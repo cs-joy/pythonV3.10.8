74148b3924b9923baf9a6b2539923820756a2d02
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-11 00:03:59 +0600

    Update ExceptionChaining.py

--- errors_and_exceptions/ExceptionChaining.py
+++ errors_and_exceptions/ExceptionChaining.py
@@ -4,10 +4,12 @@ If an unhandled exception occurs inside an except section,
 it will have the exception being handled attached to it and included in the error message:
 '''
 
+'''
 try:
     open('database.txt')
 except OSError:
     raise RuntimeError('Unable to handle error')
+'''
 
 '''
 Traceback (most recent call last):
@@ -27,11 +29,40 @@ RuntimeError: Unable to handle error
 
 '''
 To indicate that an exception is a direct consequence of another, the `raise` statement allows an optional `from` clause:
+......................
+# exc must be exception instance or None.
+raise RuntimeError from exc
 '''
 
+'''
+This can be useful when you are transforming exceptions. For example:
+'''
+
+def func():
+    raise ConnectionError
 
 
+try:
+    func()
 
+except ConnectionError as exc:
+    raise RuntimeError('Failed to open database') from exc
+
+'''
+Traceback (most recent call last):
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\ExceptionChaining.py", line 46, in <module>
+    func()
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\ExceptionChaining.py", line 42, in func
+    raise ConnectionError
+ConnectionError
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "e:\GitHub\pythonV3.10.8\errors_and_exceptions\ExceptionChaining.py", line 49, in <module>
+    raise RuntimeError('Failed to open database') from exc
+RuntimeError: Failed to open database
+'''
 
 
 

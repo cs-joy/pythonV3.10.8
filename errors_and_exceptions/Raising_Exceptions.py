bbdf1ec5e3b6b6a33622f262ac915517915fddab
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-10 23:38:25 +0600

    Update Raising_Exceptions.py

--- errors_and_exceptions/Raising_Exceptions.py
+++ errors_and_exceptions/Raising_Exceptions.py
@@ -1,1 +1,20 @@
 # refff: https://docs.python.org/3/tutorial/errors.html#raising-exceptions
+
+'''
+The `raise` statement allows the programmer to force a specified exception to occur. For example:
+'''
+
+
+# raise NameError('Hello There')
+
+'''
+The sole argument to raise indicates the exception to be raised. This must be either an exception instance or an exception class (a class that derives from BaseException, such as Exception or one of its subclasses).
+If an exception class is passed, it will be implicitly instantiated by calling its constructor with no arguments:
+'''
+
+# raise ValueError # shorthand for 'raise ValueError()'
+
+
+'''
+
+'''

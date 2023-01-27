fb24bd7ec8810254720c9c6943d524564720f495
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-27 23:35:25 +0600

    init for create "x" "w" "a"

---
+++ file_handling/write/create_files/create_files.py
@@ -0,0 +1,14 @@
+# "x" - Create - will create a file, returns an error if the file exist
+
+# "a" - Append - will create a file if the specified file does not exist
+
+# "w" - Write - will create a file if the specified file does not exist
+
+
+
+# Create a file called "demo.txt":
+#myfile = open('E:/GitHub/pythonV3.10.8/file_handling/write/create_files/demo.txt', 'x') # a new empty file is created in the following location!
+
+
+# Create a new file if it does not exist:
+new_file = open('E:/GitHub/pythonV3.10.8/file_handling/write/create_files/new.txt', 'w')

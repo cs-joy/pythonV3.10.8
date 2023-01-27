657eccfa9644a613976c042420f169a154143e88
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-27 22:43:43 +0600

    read

--- file_handling/intro.py
+++ file_handling/intro.py
@@ -1,2 +1,3 @@
 
 file = open("E:/GitHub/pythonV3.10.8/file_handling/clone_create.txt", "xt")
+
---
+++ file_handling/reading_files/reading_files.py
@@ -0,0 +1,3 @@
+file = open("E:/GitHub/pythonV3.10.8/file_handling/reading_files/read_file.txt", "a")
+
+print(file.read())

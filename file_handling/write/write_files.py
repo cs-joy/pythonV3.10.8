d29461ed5464bae9ec50389aec4005815e19ca1d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-27 23:22:46 +0600

    write - "w"

--- file_handling/write/write_files.py
+++ file_handling/write/write_files.py
@@ -1,9 +1,16 @@
+# "w" - Write - will overwrite any existing content
+
 f = open('E:/GitHub/pythonV3.10.8/file_handling/write/write_data.txt', 'w')
 
-f.write('say, hello to the negotitation world')
+f.write('"Woops! I have deleted the old content and overwrite the this content!"')
 f.close()
 
 
 file = open('E:/GitHub/pythonV3.10.8/file_handling/write/write_data.txt', 'r')
 print(file.read())
 file.close()
+
+
+'''
+Note::::: the "w" method will overwrite the entire file.
+'''

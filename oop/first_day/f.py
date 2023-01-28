522e283343f92509c41f67710e934b95eae43623
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-28 22:58:59 +0600

    Create f.py

---
+++ oop/first_day/f.py
@@ -0,0 +1,12 @@
+class Item:
+    def calculate_area(self, x, y):
+        return x * y
+
+item1 = Item()
+
+# two attributes
+# 1. height 2. width
+item1.height = 7.34
+item1.width = 9.15
+
+print(item1.calculate_area(item1.height, item1.width))

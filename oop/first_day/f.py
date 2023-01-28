2759917b6f1a826e1a5f5a8ce65d6cf91b07a32d
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-01-28 23:27:51 +0600

    Update f.py

--- oop/first_day/f.py
+++ oop/first_day/f.py
@@ -1,23 +1,23 @@
 class Item:
     # magic method - __init__()
-    def __init__(self, name):
-        print(f"An instance created: {name}")
+    def __init__(self, name, price, quantity=0):
         self.name = name # dynamic attribute assignment
+        self.price = price
+        self.quatity = quantity
+    
 
-    def calculate_total_price(self, x, y):
-        return x * y
-
-item1 = Item("Phone")
-# two attributes
-# 1. price 2. quantity
-item1.price = 700
-item1.quantity = 9
+    def calculate_total_price(self):
+        return self.price * self.quatity
 
 
 
-item2 = Item("Laptop")
-item2.price = 700
-item2.quantity = 9
+item1 = Item("Phone", 700, 9)
 
-print(item1.name)
-print(item2.name)
+
+item2 = Item("Laptop", 30000, 5)
+
+
+print(item1.calculate_total_price())
+
+print(item2.calculate_total_price())
+

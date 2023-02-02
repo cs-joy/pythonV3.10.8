4d6e18ee26cd22c91167efc33ba48319eb90efd4
cpy-script <zoyzahangir@gmail.com>
2023-02-02 09:35:19 +0600

    with csv

--- v3.11/oop/cobj.py
+++ v3.11/oop/cobj.py
@@ -18,7 +18,7 @@ student1 = Student("Md. Zahangir Alam", 202010019, "CSE")
 student2 = Student("Md. Nayem Hossain", 202011034, "CSE")
 student3 = Student("Md. Rupok Ahmed", 202021909, "CSE")
 student4 = Student("Md. Rashid", 20210219, "CS")
-student5 = Student("Md. Maruf Alam", 2020109, "CSE")
+student5 = Student("Md. Maruf Hasan", 2020109, "CSE")
 student6 = Student("Md. Nasim Alam", 2023019, "CSE")
 student7 = Student("Md. Kawsar Ahmed", 20181019, "BBA")
 
